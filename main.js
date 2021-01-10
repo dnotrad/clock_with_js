89d15fbe0910fbe808187a2bc0fd068560323c85
dnotrad <64872689+dnotrad@users.noreply.github.com>
2021-01-10 13:19:48 +0300

    audio sound

--- main.js
+++ main.js
@@ -4,29 +4,24 @@ let addTime = (type, elem) => {
     elem.innerHTML = time;
 }
 
+audioObj = new Audio('clock-audio.mp3');
+
 const updateTime = () => {
     let hoursEl = document.querySelector(".hours");
     let minutesEl = document.querySelector(".minutes");
     let secondsEl = document.querySelector(".seconds");
 
-    // let hours = new Date().getHours();
-    // if (hours < 10) hours = `0${hours}`;
-    // hoursEl.innerHTML = hours;
+    let date = new Date();
 
-    // let minutes = new Date().getMinutes();
-    // if (minutes < 10) minutes = `0${minutes}`;
-    // minutesEl.innerHTML = minutes;
+    addTime( ()=> date.getHours(), hoursEl);
+    addTime( ()=> date.getMinutes(), minutesEl);
+    addTime( ()=> date.getSeconds(), secondsEl);
 
-    // let seconds = new Date().getSeconds();
-    // if (seconds < 10) seconds = `0${seconds}`;
-    // secondsEl.innerHTML = seconds;
-
-    addTime( ()=> new Date().getHours(), hoursEl);
-    addTime( ()=> new Date().getMinutes(), minutesEl);
-    addTime( ()=> new Date().getSeconds(), secondsEl);
-
+    audioObj.play().catch((error) => {
+        console.log(error);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    setInterval( updateTime, 1000 )
+    setInterval( updateTime, 1000 );
 } )
